3b04f4678dd7ed36f52b4a0dd8b4f97a913fa5a2
longmaoaiali <13213102491@163.com>
2020-11-13 18:44:35 +0800

    come true receiver file and connect

--- app/src/main/java/com/cvte/wifip2pfiletransfer/ReceiveFileActivity.java
+++ app/src/main/java/com/cvte/wifip2pfiletransfer/ReceiveFileActivity.java
@@ -9,16 +9,21 @@ import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.cvte.wifip2pfiletransfer.Base.BaseActivity;
 import com.cvte.wifip2pfiletransfer.broadcast.DirectBroadcastReceiver;
 import com.cvte.wifip2pfiletransfer.callback.DirectActionListener;
+import com.cvte.wifip2pfiletransfer.model.FileTransfer;
 import com.cvte.wifip2pfiletransfer.service.WifiServerService;
 
+import java.io.File;
 import java.util.Collection;
 
 /**
@@ -27,55 +32,105 @@ import java.util.Collection;
 
 public class ReceiveFileActivity extends BaseActivity {
 
+    private static final String TAG = "ReceiveFileActivity";
     private ImageView mCover;
     private Button mCreateGroup;
     private Button mRemoveGroup;
     private TextView mTvLog;
     private ProgressDialog mProgressDialog;
     private WifiP2pManager mWifiP2pManager;
+    private WifiServerService mWifiServerService;
 
 
     private ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             //todo:成功绑定文件传输的服务
+            WifiServerService.MyBinder binder = (WifiServerService.MyBinder) service;
+            mWifiServerService = binder.getService();
+            mWifiServerService.setOnProgressChangListener(new WifiServerService.OnProgressChangListener() {
+                @Override
+                public void onProgressChanged(final FileTransfer fileTransfer, final int progress) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mProgressDialog.setMessage("文件名："+new File(fileTransfer.getFilePath()).getName());
+                            mProgressDialog.setProgress(progress);
+                            mProgressDialog.show();
+                        }
+                    });
+                }
+
+                @Override
+                public void onTransferFinished(final File file) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mProgressDialog.cancel();
+                            if (file != null && file.exists()) {
+                                Glide.with(ReceiveFileActivity.this).load(file.getPath()).into(mCover);
+                            }
+                        }
+                    });
+                }
+            });
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
-
+            mWifiServerService = null;
+            bindService();
         }
     };
+    private boolean mConnectionInfoAvailable = false;
+
+    private void log(String log) {
+        mTvLog.append(log + "\n");
+        mTvLog.append("----------" + "\n");
+    }
 
     private DirectActionListener mDirectActionListener = new DirectActionListener() {
         @Override
         public void setWifiP2pEnabled(boolean enabled) {
-
+            log("wifiP2pEnabled: " + enabled);
         }
 
         @Override
         public void onConnectionInfoAvailable(WifiP2pInfo wifiP2pInfo) {
-
+            log("onConnectionInfoAvailable");
+            log("isGroupOwner：" + wifiP2pInfo.isGroupOwner);
+            log("groupFormed：" + wifiP2pInfo.groupFormed);
+            if (wifiP2pInfo.groupFormed && wifiP2pInfo.isGroupOwner) {
+                mConnectionInfoAvailable = true;
+                if (mWifiServerService != null) {
+                    startService(WifiServerService.class);
+                }
+            }
         }
 
         @Override
         public void onDisconnection() {
-
+            mConnectionInfoAvailable = false;
+            log("onDisconnection");
         }
 
         @Override
         public void onSelfDeviceAvailable(WifiP2pDevice wifiP2pDevice) {
-
+            log("onSelfDeviceAvailable");
+            log(wifiP2pDevice.toString());
         }
 
         @Override
         public void onPeersAvailable(Collection<WifiP2pDevice> wifiP2pDeviceList) {
-
+            log("onPeersAvailable,size:" + wifiP2pDeviceList.size());
+            for (WifiP2pDevice wifiP2pDevice : wifiP2pDeviceList) {
+                log(wifiP2pDevice.toString());
+            }
         }
 
         @Override
         public void onChannelDisconnected() {
-
+            log("onChannelDisconnected");
         }
     };
     private WifiP2pManager.Channel mChannel;
@@ -86,6 +141,7 @@ public class ReceiveFileActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receivefile);
         initView();
+        initViewListener();
 
         mWifiP2pManager = (WifiP2pManager) getSystemService(WIFI_P2P_SERVICE);
         if (mWifiP2pManager == null) {
@@ -98,6 +154,52 @@ public class ReceiveFileActivity extends BaseActivity {
         mDirectBroadcastReceiver = new DirectBroadcastReceiver(mWifiP2pManager,mChannel,mDirectActionListener);
         registerReceiver(mDirectBroadcastReceiver,DirectBroadcastReceiver.getIntentFilter());
         bindService();
+    }
+
+    private void initViewListener() {
+        mCreateGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWifiP2pManager.createGroup(mChannel, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        log("createGroup onSuccess");
+                        dismissLoadingDialog();
+                        showToast("onSuccess");
+                    }
+
+                    @Override
+                    public void onFailure(int reason) {
+                        log("createGroup onFailure: " + reason);
+                        dismissLoadingDialog();
+                        showToast("onFailure");
+                    }
+                });
+            }
+        });
+
+        mRemoveGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeGroup();
+            }
+        });
+    }
+
+    private void removeGroup() {
+        mWifiP2pManager.removeGroup(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                log("removeGroup onSuccess");
+                showToast("onSuccess");
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                log("removeGroup onFailure");
+                showToast("onFailure");
+            }
+        });
     }
 
     private void bindService() {
@@ -122,6 +224,17 @@ public class ReceiveFileActivity extends BaseActivity {
 
     }
 
-
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mWifiServerService != null) {
+            mWifiServerService.setOnProgressChangListener(null);
+            unbindService(mServiceConnection);
+        }
+        unregisterReceiver(mDirectBroadcastReceiver);
+        stopService(new Intent(this, WifiServerService.class));
+        if (mConnectionInfoAvailable) {
+            removeGroup();
+        }
+    }
 }
